6e785ddd039af28a2427b1ab0178abcbe5290d75
Sawanjkotian <sawan9kotian@gmail.com>
2023-11-11 15:25:17 +0530

    Socket.io

--- src/component/Editor.jsx
+++ src/component/Editor.jsx
@@ -5,18 +5,64 @@ import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
 import { Box } from '@mui/material';
+import styled from '@emotion/styled';
 
+import { io } from 'socket.io-client';
+
+const Component = styled.div`
+    background: #F5F5F5
+`
+
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+  
+  var quill = new Quill('#editor', {
+    modules: {
+      toolbar: toolbarOptions
+    },
+    theme: 'snow'
+  });
 
 const Editor = () =>{
 
     useEffect(()=>{
         const quillserver = new Quill('#container', { 
-            theme: 'snow' 
+            theme: 'snow',
+            modules:{
+                toolbar:toolbarOptions
+            }
         })
-    }, [])
+    }, []);
+
+    useEffect(()=>{
+        const socket = io('http://localhost:9000');
+
+        return () =>{
+            socket.disconnect();
+        }
+    });
 
     return(
-        <Box id="container"></Box>
+        <Component>
+            <Box className='container' id="container"></Box>
+        </Component>
     )
 }
 
